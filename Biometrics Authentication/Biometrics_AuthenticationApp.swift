1d57a6e473c2db6eece2ca9403489c50cb3c5aff
mcantop <maciejcantop@icloud.com>
2022-09-12 09:09:02 +0200

    Initial Commit

---
+++ Biometrics Authentication/Biometrics_AuthenticationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Biometrics_AuthenticationApp.swift
+//  Biometrics Authentication
+//
+//  Created by Maciej on 12/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Biometrics_AuthenticationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
